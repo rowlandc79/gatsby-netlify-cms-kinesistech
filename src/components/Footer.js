b713e00323a20e2e8ca82421332d4a7c5d8118cc
Rowland Corentin <rowland.corentin@icloud.com>
2021-05-15 14:27:05 +0100

    remove unnecessary bits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,12 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          <img
+          {/* <img
             src={logo}
-            alt="Kaldi"
+            alt=""
             style={{ width: '14em', height: '10em' }}
-          />
+          /> */}
+          <p span="color:purple;font-weight:bold;font-size:24px">Kinesis Technologies</p>
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,7 +43,7 @@ const Navbar = class extends React.Component {
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
               {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
-              <h3>Kinesis Technologies</h3>
+              <p span="color:purple;font-weight:bold;">Kinesis Technologies</p>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -73,9 +73,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
+              {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link> */}
             </div>
             <div className="navbar-end has-text-centered">
               <a
