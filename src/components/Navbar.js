ea47293721e19168e46bb7d2f8d5ab135c7fc7b1
Rowland Corentin <rowland.corentin@icloud.com>
2021-05-15 15:35:09 +0100

    logo fixup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/kinesis-technologies-logo-e1561224154805.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
+              {/* <img src={logo} alt="Kinesis Technologies" style={{ width: '88px' }} /> */}
               <p span="color:purple;font-weight:bold;">Kinesis Technologies</p>
             </Link>
             {/* Hamburger menu */}
