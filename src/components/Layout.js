2494c84fddb2152e92cf280e20900eb8e39c3ff5
Rowland Corentin <rowland.corentin@icloud.com>
2021-05-18 15:32:10 +0100

    og improvements

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/vectorstock_34176008.png`}
+          content={`${withPrefix('/')}img/vectorstock_34176008_1200630.png`}
         />
       </Helmet>
       <Navbar />
