33d36c0ffc1eb8322767646b2b5fcd67d38767ad
Rowland Corentin <rowland.corentin@icloud.com>
2021-05-18 15:36:55 +0100

    remove logo svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -12,11 +11,6 @@ const Footer = class extends React.Component {
     return (
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
-          {/* <img
-            src={logo}
-            alt=""
-            style={{ width: '14em', height: '10em' }}
-          /> */}
           <p span="color:purple;font-weight:bold;font-size:24px">Kinesis Technologies</p>
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
